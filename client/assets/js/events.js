f7756607ca60b3a93793e5b2a650c77dd0dc99c8
Florian B <floryn93@gmail.com>
2019-01-13 23:27:28 +0200

    [master] Add client events.

---
+++ client/assets/js/events.js
@@ -0,0 +1,17 @@
+/**
+ * Client events.
+ *
+ * @type {Object}
+ */
+const events = Object.freeze({
+	watchMode: 'watchModeEventHandler',
+	players: 'playersEventHandler',
+	error: 'errorEventHandler',
+	success: 'successEventHandler',
+	playerId: 'playerIdEventHandler',
+	playerColor: 'playerColorEventHandler',
+	activePlayer: 'activePlayerEventHandler',
+	table: 'tableEventHandler',
+	gameOver: 'gameOverEventHandler',
+	gameStatus: 'gameStatusEventHandler'
+});
