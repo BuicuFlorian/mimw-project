55f638914a3b375291ff2a4897e30b194426681c
Florian B <floryn93@gmail.com>
2019-01-13 23:29:39 +0200

    [master] Add helper function to create the table.

---
+++ client/assets/js/createTable.js
@@ -0,0 +1,40 @@
+if (storage['table']()) {
+	(function createTable() {
+    const table = JSON.parse(storage['table']()));
+		const tableResponsive = document.createElement('div');
+		tableResponsive.classList = 'table-responsive mt-5';
+
+		const newTable = document.createElement('table');
+		newTable.classList = 'table table-bordered';
+
+		const tableBody = document.createElement('tbody');
+		newTable.appendChild(tableBody);
+
+		const tableLength = table.length;
+
+		for (let row = 0; row < tableLength; row++) {
+			let rowLength = table[row].length;
+			let tr = document.createElement('tr');
+			tableBody.appendChild(tr)
+
+			for (let col = 0; col < rowLength; col++) {
+				let td = document.createElement('td');
+				td.id = `[${row},${col}]`;
+				td.onclick = function () {
+					getPosition(this.id);
+				};
+
+				if (table[row][col] !== 0) {
+					td.style.background = table[row][col];
+				}
+
+				tr.appendChild(td);
+			}
+		}
+
+		tableResponsive.appendChild(newTable);
+
+		let playground = document.getElementById('playground');
+		playground.appendChild(tableResponsive);
+	})();
+}
