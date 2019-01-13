13a704f0b3911e206f46c404abda06fe32a283fc
Florian B <floryn93@gmail.com>
2019-01-13 23:27:50 +0200

    [master] Add web socket client.

---
+++ client/assets/js/main.js
@@ -0,0 +1,22 @@
+const url = 'ws://localhost:8080';
+const connection = new WebSocket(url);
+
+connection.onopen = () => {
+  console.log('Connected to the server.');
+  connection.send(JSON.stringify({ text: 'New client connected!'}));
+};
+
+connection.onmessage = event => {
+  const data = JSON.parse(event.data);
+  const keys = Object.keys(data);
+
+  keys.forEach(key => {
+    if (events.hasOwnProperty(key)) {
+      window[events[key]](data);
+    }
+  });
+};
+
+connection.onerror = error => {
+  console.log(`WebSocket error: ${error}`);
+};
