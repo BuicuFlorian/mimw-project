cc86c3fd824502ba6e06028044d6edf8d509a457
Florian B <floryn93@gmail.com>
2019-01-13 23:24:40 +0200

    [master] Add server events.

---
+++ server/config/events.js
@@ -0,0 +1,12 @@
+/**
+ * Server events.
+ *
+ * @type {Object}
+ */
+const EVENTS = Object.freeze({
+  JOIN_GAME: 'joinGame',
+  WATCH_GAME: 'watchGame',
+  MOVE: 'move'
+});
+
+module.exports = EVENTS;
