94baa533920e34ff9d2d349f88e9b5e888577e9d
Florian B <floryn93@gmail.com>
2019-02-02 14:38:05 +0200

    [master] Implement jokers functionality.

--- server/controllers/game.controller.js
+++ server/controllers/game.controller.js
@@ -34,29 +34,58 @@ class GameController extends InfoController {
    * @returns {Array}
    */
   updateTable(data) {
+    const player = super.getPlayerById(data.playerId);
+    let joker = null;
+
+    if (data.joker) {
+      if (this.gameInfo.players[player]['jokers'][data.joker] === true) {
+        joker = data.joker;
+      } else {
+        throw new Error('You already used that joker.');
+      }
+    }
+
     const playerColor = super.getPlayerColor(data.playerId);
     const position = JSON.parse(data.position);
     const row = position[0];
     const col = position[1];
-    const isValidPosition = this.validatePosition(row, col, playerColor);
+    const isValidPosition = this.validatePosition(row, col, playerColor, joker);
 
     if (isValidPosition) {
       this.gameInfo.table[row][col] = playerColor;
 
-      const nextPlayer = this.activateNextPlayer(data.playerId);
-      console.log('Blocked:', this.isBlocked(playerColor));
-      if (this.isBlocked(playerColor)) {
-        const player = super.getPlayerById(data.playerId);
+      if (joker) {
+        if (joker === 'double_move') {
+          this.gameInfo.table[row][col] = 0;
+          this.doubleMove(row, col, playerColor);
+          this.gameInfo.table[row][col] = playerColor;
+        }
+
+        this.gameInfo.players[player]['jokers'][joker] = false;
+      }
+
+      if (this.isBlocked(playerColor, data.playerId)) {
         this.gameInfo.players[player]['blocked'] = true;
       };
+
+      const nextPlayer = this.activateNextPlayer(data.playerId);
+
+      if (this.isBlocked(this.gameInfo.players[nextPlayer]['color'], data.playerId)) {
+        this.gameInfo.players[player]['blocked'] = true;
+        if (super.getPlayers().length > 1) {
+          super.updateGameInfo();
+          data.playerId = this.gameInfo.players[nextPlayer]['id'];
+          return this.updateTable(data);
+        }
+      }
 
       super.updateGameInfo();
 
       let gameInfo = { table: this.gameInfo.table };
 
       if (super.getPlayers().length === 1) {
-        gameInfo.gameOver = true
-        gameInfo.gameStatus = super.getGameStatus()
+        gameInfo.gameOver = true;
+        gameInfo.gameStatus = super.getGameStatus();
 
         setTimeout(() => {
           super.clearGameInfo();
@@ -70,31 +99,103 @@ class GameController extends InfoController {
   }
 
   /**
+   * Color the missing cell when the player uses double_move joker.
+   *
+   * @param {Number} row
+   * @param {Number} col
+   * @param {String} playerColor
+   */
+  doubleMove(row, col, playerColor) {
+    const rowsLength = this.gameInfo.table.length - 1;
+    const colsLength = this.gameInfo.table[0].length - 1;
+
+    if ((col - 2) >= 0 && this.gameInfo.table[row][col - 2] === playerColor) {
+      if (this.gameInfo.table[row][col - 1] !== 0) {
+        throw new Error('Invalid position, try again!');
+      }
+      this.gameInfo.table[row][col - 1] = playerColor;
+    } else if ((col + 2) <= colsLength && this.gameInfo.table[row][col + 2] === playerColor) {
+      if (this.gameInfo.table[row][col + 1] !== 0) {
+        throw new Error('Invalid position, try again!');
+      }
+
+      this.gameInfo.table[row][col + 1] = playerColor;
+    } else if ((row - 2) >= 0 && this.gameInfo.table[row - 2][col] === playerColor) {
+      if (this.gameInfo.table[row - 1][col] !== 0) {
+        throw new Error('Invalid position, try again!');
+      }
+
+      this.gameInfo.table[row - 1][col] = playerColor;
+    } else if ((row + 2) <= rowsLength && this.gameInfo.table[row + 2][col] === playerColor) {
+      if (this.gameInfo.table[row + 1][col] !== 0) {
+        throw new Error('Invalid position, try again!');
+      }
+
+      this.gameInfo.table[row + 1][col] = playerColor;
+    }
+  }
+
+  /**
    * Validate the given position.
    *
    * @param {Number} row
    * @param {Number} col
    * @param {String} playerColor
+   * @param {String} joker
    * @return {Boolean}
    */
-  validatePosition(row, col, playerColor) {
+  validatePosition(row, col, playerColor, joker) {
+    if (joker) {
+      return this.validateJokerMove(row, col, playerColor, joker);
+    }
+
     if (this.gameInfo.table[row][col] === 0) {
-      return this.crossValidate(row, col, playerColor);
+      return this.crossValidate(row, col, playerColor, 1);
     }
 
     return false;
   }
 
-  isBlocked(playerColor) {
+  validateJokerMove(row, col, playerColor, joker) {
+    if (joker === 'double_move' && this.gameInfo.table[row][col] === 0) {
+      return this.crossValidate(row, col, playerColor, 2);
+    }
+
+    if (joker === 'replacement' && this.gameInfo.table[row][col] !== playerColor) {
+      return this.crossValidate(row, col, playerColor, 1);
+    }
+
+    if (joker === 'freedom' && this.gameInfo.table[row][col] === 0) {
+      return true;
+    }
+
+    return false;
+  }
+
+  isBlocked(playerColor, playerId) {
+    const jokers = super.getPlayerJokers(playerId);
+    const validJokers = this.validateJokers(JSON.parse(jokers));
     const rowsLength = this.gameInfo.table.length - 1;
     const colsLength = this.gameInfo.table[0].length - 1;
     let validPositions = [];
 
     for (let row = 0; row <= rowsLength; row++) {
       for (let col = 0; col <= colsLength; col++) {
-        if (this.gameInfo.table[row][col] === playerColor) {
-          if (this.crossValidate(row, col, 0) === true) {
+        if (validJokers.includes('freedom') || validJokers.includes('replacement')) {
+          if (validJokers.includes('freedom')
+            && this.gameInfo.table[row][col] === 0) {
             validPositions.push([row, col]);
+          } else if (validJokers.includes('replacement')
+            && this.gameInfo.table[row][col] !== playerColor) {
+            if (this.crossValidate(row, col, playerColor, 1) === true) {
+              validPositions.push([row, col]);
+            }
+          }
+        } else {
+          if (this.gameInfo.table[row][col] === playerColor) {
+            if (this.crossValidate(row, col, 0, 1) === true) {
+              validPositions.push([row, col]);
+            }
           }
         }
       }
@@ -103,25 +204,38 @@ class GameController extends InfoController {
     return validPositions.length === 0;
   }
 
+  validateJokers(jokers) {
+    let validJokers = [];
+
+    for (let joker in jokers) {
+      if (jokers[joker] === true) {
+        validJokers.push(joker)
+      }
+    }
+
+    return validJokers;
+  }
+
   /**
    * Cross validate the given position.
    *
    * @param {Number} row
    * @param {Number} col
    * @param {Number|String} criteria
+   * @param {Number} size
    * @return {Boolean}
    */
-  crossValidate(row, col, criteria) {
+  crossValidate(row, col, criteria, size) {
     const rowsLength = this.gameInfo.table.length - 1;
     const colsLength = this.gameInfo.table[0].length - 1;
 
-    if ((col + 1) <= colsLength && this.gameInfo.table[row][col + 1] === criteria) {
+    if ((col + size) <= colsLength && this.gameInfo.table[row][col + size] === criteria) {
       return true;
-    } else if ((col - 1) >= 0  && this.gameInfo.table[row][col - 1] === criteria) {
+    } else if ((col - size) >= 0 && this.gameInfo.table[row][col - size] === criteria) {
       return true;
-    } else if ((row + 1) <= rowsLength && this.gameInfo.table[row + 1][col] === criteria) {
+    } else if ((row + size) <= rowsLength && this.gameInfo.table[row + size][col] === criteria) {
       return true;
-    } else if ((row - 1) >= 0 && this.gameInfo.table[row - 1][col] === criteria) {
+    } else if ((row - size) >= 0 && this.gameInfo.table[row - size][col] === criteria) {
       return true;
     }
 
